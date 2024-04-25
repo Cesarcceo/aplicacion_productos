cf31b41c6a61f199b4119104fc43d48ff340b3db
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-04-25 14:23:41 -0600

    Initial commit :tada:

---
+++ android/app/src/main/kotlin/com/example/aplicacion_productos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aplicacion_productos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/screens.dart
@@ -0,0 +1,4 @@
+
+export 'package:aplicacion_productos/screens/home_screen.dart';
+export 'package:aplicacion_productos/screens/login_sreen.dart';
+
