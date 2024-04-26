fcae2707d0125a03841650cf3776d521e1be8978
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-04-26 17:19:18 -0600

    New feature (funcionalidad  y validacion): :sparkles: los numeros ya estan validados y el switch ya funciona

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,7 +1,8 @@
-import 'package:aplicacion_productos/providers/product_from_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
+import 'package:aplicacion_productos/providers/product_from_provider.dart';
 import 'package:aplicacion_productos/services/product_service.dart';
 import 'package:aplicacion_productos/ui/input_decorations.dart';
 import 'package:aplicacion_productos/widgets/widgets.dart';
@@ -33,6 +34,7 @@ class _ProductsScreenBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         child: Column(
           children: [
     
@@ -96,12 +98,13 @@ class _ProductForm extends StatelessWidget {
         child: Form(
           child: Column(
             children: [
-              SizedBox(height: 30,),
+              const SizedBox(height: 30,),
               TextFormField(
                 initialValue: product.name,
                 onChanged: (value) => product.name = value,
                 validator: (value) {
-                  if (value == null || value.isEmpty ) return 'El nombre es Obligatorio';
+                  if (value == null || value.isEmpty ) 
+                    return 'El nombre es Obligatorio';
                 },
                 decoration: inputDecorations.authInputDecorations(
                   hintText: 'Nombre del producto', 
@@ -110,9 +113,13 @@ class _ProductForm extends StatelessWidget {
       
               ),
       
-              SizedBox(height: 30,),
+              const SizedBox(height: 30,),
               TextFormField(
                 initialValue: '${product.price}',
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'^(\d+)?\.?\d{0,2}'))
+
+                ],
                 onChanged: (value) {
                   if(double.tryParse(value)==null){
                     product.price=0;
@@ -127,16 +134,15 @@ class _ProductForm extends StatelessWidget {
                 ),
       
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
+
               SwitchListTile(
                 value: product.available, 
-                title: Text('Disponible'),
+                title: const Text('Disponible'),
                 activeColor: Colors.indigo,
-                onChanged: (value){
-      
-                }
+                onChanged: productForm.updateAvailability
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
             ],
           ), 
         ),
@@ -147,11 +153,11 @@ class _ProductForm extends StatelessWidget {
   BoxDecoration _buildBoxDecoration() {
     return BoxDecoration(
       color: Colors.white,
-      borderRadius: BorderRadius.only(bottomRight: Radius.circular(25), bottomLeft: Radius.circular(25)),
+      borderRadius: const BorderRadius.only(bottomRight: Radius.circular(25), bottomLeft: Radius.circular(25)),
       boxShadow: [
         BoxShadow(
         color: Colors.black.withOpacity(0.05),
-        offset: Offset(0,5),
+        offset: const Offset(0,5),
         blurRadius: 5,
         )
       ]
