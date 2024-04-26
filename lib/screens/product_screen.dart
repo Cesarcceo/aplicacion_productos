5f0a26d1449e478fab25441b9062e0e2d44ed647
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-04-26 12:50:25 -0600

    New feature (product_screen.dart): :sparkles: se agregaron barras de texto y una barra de estado para editar la informacion del producto en la pantalla del producto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aplicacion_productos/ui/input_decorations.dart';
 import 'package:aplicacion_productos/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -10,14 +11,109 @@ class ProductScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
+
             Stack(
               children: [
                 ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: Icon(Icons.arrow_back, size: 40,color: Colors.white),
+                  ),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 20,
+                  child: IconButton(
+                    onPressed: () {
+                      //camara, galeria
+                    },
+                    icon: Icon(Icons.camera_alt, size: 40,color: Colors.white),
+                  ),
+                ),
               ],
-            )
+            ),
+
+            _ProductForm(),
+
+            const SizedBox(height: 100),
+
           ],
         ),
-      )
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        child:const Icon(Icons.save),
+        onPressed: (){
+
+        },
+      ),
    );
+  }
+}
+
+class _ProductForm extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          child: Column(
+            children: [
+              SizedBox(height: 30,),
+              TextFormField(
+                decoration: inputDecorations.authInputDecorations(
+                  hintText: 'Nombre del producto', 
+                  labelText: 'Nombre',
+                ),
+      
+              ),
+      
+              SizedBox(height: 30,),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: inputDecorations.authInputDecorations(
+                  hintText: '\$150', 
+                  labelText: 'precio',
+                ),
+      
+              ),
+              SizedBox(height: 30),
+              SwitchListTile(
+                value: true, 
+                title: Text('Disponible'),
+                activeColor: Colors.indigo,
+                onChanged: (value){
+      
+                }
+              ),
+              SizedBox(height: 30),
+            ],
+          ), 
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.only(bottomRight: Radius.circular(25), bottomLeft: Radius.circular(25)),
+      boxShadow: [
+        BoxShadow(
+        color: Colors.black.withOpacity(0.05),
+        offset: Offset(0,5),
+        blurRadius: 5,
+        )
+      ]
+    );
   }
 }
