4b76275c0aa88d92800bafd6611291447d187018
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-04-26 11:08:46 -0600

    New feature (product_card.dart): :sparkles: se terminaron de estructurar los detalles de la tarjeta

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProductCard extends StatelessWidget {
 
@@ -17,6 +18,19 @@ class ProductCard extends StatelessWidget {
             _BackgrounImage(),
 
             _ProductDetails(),
+
+            Positioned(
+              top: 0,
+              right: 0,
+              child: _PriceTag()
+            ),
+
+            //TODO: mostrar de manera condicional
+            const Positioned(
+              top: 0,
+              left: 0,
+              child: _NotAvailable()
+            )
           ],
         ),
       ),
@@ -39,15 +53,91 @@ class ProductCard extends StatelessWidget {
   }
 }
 
-class _ProductDetails extends StatelessWidget {
+class _NotAvailable extends StatelessWidget {
+  const _NotAvailable({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
+      width: 100,
       height: 70,
-      color: Colors.red,
+      decoration: BoxDecoration(
+        color: Colors.yellow[800],
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(25), bottomRight: Radius.circular(25))
+      ),
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'No disponible',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class _PriceTag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(topRight: Radius.circular(25), bottomLeft: Radius.circular(25))
+      ),
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text('\$103.99', style: TextStyle(color:  Colors.white, fontSize: 20),),
+        ),
+      ),
+    );
+  }
+}
+
+class _ProductDetails extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 70),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 80,
+        decoration: _buildBoxDecoration(),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Disco duro C', 
+              style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              'Id Del Disco duro', 
+              style: TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() {
+    return BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25), topRight: Radius.circular(25))
+      );
   }
 }
 
